8ee6c367452b705581ce16bb8b1fb015b5ccff72
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-09 19:46:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -0,0 +1,40 @@
+// User function template for C++
+
+class Solution {
+  public:
+  bool check(int mid ,int k,vector<int> &arr){
+        int sum=0;int cnt=1;
+        for(auto x:arr){
+            if(sum+x>mid){
+                cnt++;
+                sum=x;
+            }
+            else{
+                sum+=x;
+            }
+        }
+        return cnt<=k;
+    }
+    int minTime(vector<int>& arr, int k) {
+       int low=*max_element(arr.begin(),arr.end());
+       int high=accumulate(arr.begin(),arr.end(),0);
+       int ans=0;
+       while(low<=high){
+        int mid=low + (high-low)/2;
+        if(check(mid,k,arr)){
+            ans=mid;
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+       } 
+       return ans;
+    }
+    // int (vector<int>& arr, int k) {
+    //     // code here
+        
+    //     // return minimum time
+        
+    // }
+};
