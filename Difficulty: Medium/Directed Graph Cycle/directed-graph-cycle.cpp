4dc25dcafac1cd8cf26a6c2c080e0940c7a68b62
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-17 12:13:52 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,33 +1,114 @@
 class Solution {
   public:
-    bool dfs(int i,vector<vector<int>>&arr,vector<int>&visit,vector<int> &path){
-        visit[i]=1;
-        path[i]=1;
-        for(auto it:arr[i]){
-            if(!visit[it]){
-                if(dfs(it,arr,visit,path)){
-                    return true;
-                }
-            }
-            else if(path[it]==1)return true;
-        }
-        path[i]=0;
-        return false;
-    }
-    bool isCyclic(int V, vector<vector<int>> &edge) {
-        // code here
-        vector<int> visit(V,0);
-        vector<int> path(V,0);
-        vector<vector<int>> arr(V);
-        for(int i=0;i<edge.size();i++){
-            arr[edge[i][0]].push_back(edge[i][1]);
-        }
-        for(int i=0;i<V;i++){
-          if(!visit[i]){
-            if(dfs(i,arr,visit,path))return true;    
-          }
+    // bool dfs(int i,vector<vector<int>>&arr,vector<int>&visit,vector<int> &path){
+    //     visit[i]=1;
+    //     path[i]=1;
+    //     for(auto it:arr[i]){
+    //         if(!visit[it]){
+    //             if(dfs(it,arr,visit,path)){
+    //                 return true;
+    //             }
+    //         }
+    //         else if(path[it]==1)return true;
+    //     }
+    //     path[i]=0;
+    //     return false;
+    // }
+    // bool isCyclic(int V, vector<vector<int>> &edge) {
+    //     // code here
+    //     vector<int> visit(V,0);
+    //     vector<int> path(V,0);
+    //     vector<vector<int>> arr(V);
+    //     for(int i=0;i<edge.size();i++){
+    //         arr[edge[i][0]].push_back(edge[i][1]);
+    //     }
+    //     for(int i=0;i<V;i++){
+    //       if(!visit[i]){
+    //         if(dfs(i,arr,visit,path))return true;    
+    //       }
           
-        }
-        return false;
+    //     }
+    //     return false;
+    // }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    bool isCyclic(int V, vector<vector<int>> &edge) {
+       vector<vector<int>>arr(V);
+       vector<int> in(V,0);
+       queue<int>q;
+       for(int i=0;i<edge.size();i++){
+        arr[edge[i][0]].push_back(edge[i][1]);
+        in[edge[i][1]]++;
+       }
+       for(int i=0;i<V;i++){
+           if(in[i]==0)q.push(i);
+       }
+       while(!q.empty()){
+           int front=q.front();q.pop();
+           for(auto it:arr[front]){
+               in[it]--;
+               if(in[it]==0)q.push(it);
+           }
+       }
+       for(auto it:in){
+           if(it>0)return true;
+       }
+       return false;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
