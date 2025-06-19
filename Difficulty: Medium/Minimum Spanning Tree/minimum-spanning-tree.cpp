4588c73df6cde771f04fced30551fd637b2f766d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-19 15:25:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -1,77 +1,55 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
+class Disjoint{
+  public:
+  vector<int> size,parent;
+  Disjoint(int n){
+      size.resize(n+1,1);
+      parent.resize(n+1);
+      for(int i=0;i<=n;i++){
+          parent[i]=i;
+      }
+  }
+  int findUParent(int node){
+      if(node==parent[node])return node;
+      return parent[node]=findUParent(parent[node]);
+  }
+  void Unionbysize(int u,int v){
+      int ulp_u=findUParent(u);
+      int ulp_v=findUParent(v);
+      if(ulp_u==ulp_v)return;
+      if(size[ulp_u]<size[ulp_v]){
+          parent[ulp_u]=ulp_v;
+          size[ulp_v]+=size[ulp_u];
+      }
+      else{
+          parent[ulp_v]=ulp_u;
+          size[ulp_u]+=size[ulp_v];
+      }
+  }
+};
 class Solution {
   public:
     // Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> arr[]) {
-        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>> > q;
-        vector<int> vis(V,0);
-
-        //in this not create MST vector as it is asking for only weight if tee also aske then store 
-        //node and it parent after once it visit so that when it to show tree remove all unnecessary edge
-        q.push({0,0,-1});
-        int sum=0;
-        while(!q.empty()){
-            vector<int> temp=q.top();
-            int wt=temp[0];
-            int node=temp[1];
-            int parent=temp[2];
-            q.pop();
-            if(!vis[node]){
-              if(parent==-1){
-                vis[node]=1;
-              }
-              else{
-                vis[node]=1;
-                sum+=wt;
-                //mst.push_back({node,parent});
-              }
-              for(auto it:arr[node]){
-                if(!vis[it[0]]){
-                  q.push({it[1],it[0],node});  
-                }
-              }   
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        // code here
+        vector<pair<int,pair<int,int>>> edge;
+        for(int i=0;i<V;i++){
+            for(auto it:adj[i]){
+                int adjnode=it[0];
+                int wt=it[1];
+                edge.push_back({wt,{i,adjnode}});
             }
-        } return sum; 
+        }
+        sort(edge.begin(),edge.end());
+        int mstsum=0;
+        Disjoint check(V);
+        for(int i=0;i<edge.size();i++){
+            int u=edge[i].second.first;
+            int v=edge[i].second.second;
+            if(check.findUParent(u)!=check.findUParent(v)){
+              mstsum+=edge[i].first;
+              check.Unionbysize(u,v);
+            }
+        }
+        return mstsum;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int V, E;
-        cin >> V >> E;
-        vector<vector<int>> adj[V];
-        int i = 0;
-        while (i++ < E) {
-            int u, v, w;
-            cin >> u >> v >> w;
-            vector<int> t1, t2;
-            t1.push_back(v);
-            t1.push_back(w);
-            adj[u].push_back(t1);
-            t2.push_back(u);
-            t2.push_back(w);
-            adj[v].push_back(t2);
-        }
-
-        Solution obj;
-        cout << obj.spanningTree(V, adj) << "\n";
-
-        cout << "~"
-             << "\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
