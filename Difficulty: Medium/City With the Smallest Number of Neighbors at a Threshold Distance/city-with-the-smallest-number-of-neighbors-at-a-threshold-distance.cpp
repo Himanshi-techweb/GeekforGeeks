b16f98fbfd681a4b64480ce43142a50a1e5c465c
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-19 12:36:42 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
+++ Difficulty: Medium/City With the Smallest Number of Neighbors at a Threshold Distance/city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.cpp
@@ -1,93 +1,62 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 // User function Template for C++
 class Solution {
   public:
-    int findCity(int n, int m, vector<vector<int>>& edges, int th) {
+    int findCity(int n, int m, vector<vector<int>>& edge, int distanceThreshold) {
         // Your code here
-        // int n=mat.size();
-        vector<vector<int>> cost(n,vector<int>(n,INT_MAX));
-        // vector<vector<int>> mat(n,vector<int>(n,INT_MAX));
-        for(int i=0;i<m;i++){
-           int x=edges[i][0];
-           int y=edges[i][1];
-           int w=edges[i][2];
-           cost[x][y]=w;
-           cost[y][x]=w;
+        vector<vector<int>> cnt(n,vector<int> (n,0));
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:edge){
+            int u=it[0];
+            int v=it[1];
+            int wt=it[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
-        // for(int i=0;i<n;i++){
-        //       for(int j=0;j<n;j++){
-        //       if(mat[i][j]!=-1)cost[i][j]=mat[i][j];
-        //       else {
-        //           cost[i][j]=INT_MAX;
-        //       }
-                  
-        // }}
-        for(int k=0;k<n;k++){
-        //   vector<vector<int>> check(n,vector<int>(n));          
-          for(int i=0;i<n;i++){
-              for(int j=0;j<n;j++){
-                  if(cost[i][k]!=INT_MAX && cost[k][j]!=INT_MAX){
-                    int temp=cost[i][k]+cost[k][j];
-                  cost[i][j]=min(cost[i][j],temp);  
-                  }
-              }
-          }
-        //   cost=check;
-        }
-        int min_check=n;
-        int k=-1;
-        vector<int> ans(n,0);
         for(int i=0;i<n;i++){
-            int x=0;
-            for(int j=0;j<n;j++){
-                if(i!=j && cost[i][j]<=th){
-                  x++;  
+            int src=i;
+            //apply dijkstra for each src node 
+            vector<int> dist(n,INT_MAX);
+            priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>q;
+            q.push({0,src});
+            dist[src]=0;
+            while(!q.empty()){
+                int node=q.top().second;
+                int wt=q.top().first;
+                q.pop();
+                for(auto it:adj[node]){
+                    int adjnode=it.first;
+                    int adjwt=it.second;
+                    if(dist[adjnode]>dist[node]+adjwt){
+                        dist[adjnode]=dist[node]+adjwt;
+                        q.push({dist[node]+wt,adjnode});
+                    }
+                }
+                
+            }
+            for(int i=0;i<n;i++){
+                if(dist[i]<=distanceThreshold){
+                    cnt[src][i]++;
                 }
             }
-            if(x<min_check || (x==min_check && i>k)){
-                min_check=x;
-                k=i;
-            }
         }
-        
-        return k;
+        vector<int> total(n,0);
+        for(int i=0;i<n;i++){
+            int sum=0;
+            for(int j=0;j<n;j++){
+               sum+=cnt[i][j]; 
+            }
+            total[i]=sum;
+        }
+        // sort(total.begin(),total.end());
+        // int ans=0;
+        int ans=INT_MAX;int ix=0;
+        for(int i=0;i<n;i++){
+           if(total[i]<ans){
+               ans=total[i];
+               ix=i;
+           }
+           else if(total[i]==ans)ix=i;
+        }
+        return ix;
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> adj;
-        // n--;
-        for (int i = 0; i < m; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            adj.push_back(temp);
-        }
-
-        int dist;
-        cin >> dist;
-        Solution obj;
-        cout << obj.findCity(n, m, adj, dist) << "\n";
-    
-cout << "~" << "\n";
-}
-}
-
-// } Driver Code Ends
