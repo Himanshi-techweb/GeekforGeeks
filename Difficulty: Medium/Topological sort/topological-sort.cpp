3a69ecf13ce9b7968ff837ee77711181bc01ac9d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 15:42:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -1,33 +1,38 @@
 class Solution {
   public:
-    void dfs(int i,vector<vector<int>>&arr,vector<int>&visit,stack<int>&st){
-        visit[i]=1;
-        for(auto it:arr[i]){
-            if(!visit[it]){
-              dfs(it,arr,visit,st);  
-            }
-        }
-        st.push(i);
-    }
     vector<int> topoSort(int V, vector<vector<int>>& edge) {
         // code here
         vector<vector<int>> arr(V);
-        for(int i=0;i<edge.size();i++){
-            arr[edge[i][0]].push_back(edge[i][1]);
-            
+        vector<int> visit(V,0);
+        vector<int> result;
+        vector<int> in(V,0);
+        queue<int> q;
+        for(auto it:edge){
+            arr[it[0]].push_back(it[1]);
+            in[it[1]]++;
         }
-            stack<int> st;
-            vector<int> visit(V,0);
-            for(int i=0;i<V;i++){
-                if(!visit[i]){
-                    dfs(i,arr,visit,st);
+        for(int i=0;i<V;i++){
+            if(in[i]==0){
+                q.push(i);
+                result.push_back(i);
+                visit[i]=1;
+            }
+        }
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            for(auto it:arr[front]){
+                if(!visit[it]){
+                    in[it]--;
+                    if(in[it]==0){
+                        result.push_back(it);
+                        q.push(it);
+                        visit[it]=1;
+                    }
+                    
                 }
             }
-        vector<int> topo;
-        while(!st.empty()){
-           topo.push_back(st.top());
-           st.pop();
         }
-        return topo;
+        return result;
     }
 };
