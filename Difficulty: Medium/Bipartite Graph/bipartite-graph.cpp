30c4535dd30d37c473ed71cb01c5c472001cf0b8
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 12:44:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
+++ Difficulty: Medium/Bipartite Graph/bipartite-graph.cpp
@@ -1,28 +1,30 @@
 class Solution {
   public:
+    bool dfs(int i,int parent,vector<vector<int>> &arr,vector<int> &color,vector<int> &visit){
+        visit[i]=1;
+        for(auto it:arr[i]){
+            if(!visit[it] ){
+                color[it]=!color[i];
+                if(!dfs(it,i,arr,color,visit))return false;
+            }
+            else if(it !=parent && color[it]==color[i])return false;
+        }
+        return true;
+    }
     bool isBipartite(int V, vector<vector<int>> &edge) {
         // Code here
-        vector<vector<int>> arr(V);
-        queue<int> q;
         vector<int> color(V,-1);
-        for(int i=0;i<edge.size();i++){
-           arr[edge[i][0]].push_back(edge[i][1]);
-           arr[edge[i][1]].push_back(edge[i][0]);
+        vector<vector<int>> arr(V);
+        vector<int> visit(V,0);
+        for(auto it:edge){
+            arr[it[0]].push_back(it[1]);
+            arr[it[1]].push_back(it[0]);
         }
-        q.push(0);
-        color[0]=0;
-        while(!q.empty()){
-           int front=q.front();
-           q.pop();
-           for(auto it:arr[front]){
-               if(color[it]==-1){
-                  color[it]=(color[front]==0?1:0); 
-                  q.push(it);
-               }
-               else if(color[it]==color[front]){
-                return false;   
-               }
-           }
+        for(int i=0;i<V;i++){
+            if(!visit[i]){
+                color[i]=0;
+                if(!dfs(i,-1,arr,color,visit)) return false;
+            }
         }
         return true;
     }
