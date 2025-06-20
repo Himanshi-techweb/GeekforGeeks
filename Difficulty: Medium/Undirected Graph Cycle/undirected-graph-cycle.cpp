87292da76a0bd1495879022643ba19fe1d950cc6
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 10:53:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,34 +1,31 @@
 class Solution {
   public:
-    bool condition=false;bool condition2=false;
-    bool dfs(int src,int parent,vector<vector<int>>&arr,vector<int> &vis){
-        vis[src]=1;
-        for(auto it:arr[src]){
-            if(!vis[it]){
-                condition= dfs(it,src,arr,vis);
-            }
-            else if(it!=parent){
-                condition2=true;
-            }
-        }
-        return condition||condition2;
+    //bool condition=false;bool condition2=false;
+    bool dfs(int src,int parent,vector<vector<int>>&adj,vector<int> &visit){
+       visit[src]=1;
+       for(auto it:adj[src]){
+           if(visit[it]!=1 && it!=parent){
+              if(dfs(it,src,adj,visit))return true; 
+           }
+           else if(visit[it]==1 && it!=parent){
+               return true;
+           }
+       }
+       return false;
     }
     bool isCycle(int V, vector<vector<int>>& edge) {
         // Code here
-        int e=edge.size();
-        vector<vector<int>> arr(V);
-        for(int i=0;i<e;i++){
-          int j=edge[i][0];
-          int k=edge[i][1];
-          arr[j].push_back(k);
-          arr[k].push_back(j);
-        }
-        vector<int> vis(V,0);
-        for(int i=0;i<V;i++){
-            if(!vis[i]){
-                if(dfs(i,-1,arr,vis))return true;
-            }
-        }
-        return false;
+       vector<vector<int>> adj(V);
+       for(auto it:edge){
+           adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+       }
+       vector<int> visit(V,0);
+       for(int i=0;i<V;i++){
+           if(!visit[i]){
+               if(dfs(i,-1,adj,visit)) return true;
+           }
+       }
+       return false;
     }
 };
