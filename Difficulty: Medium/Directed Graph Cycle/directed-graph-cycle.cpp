afe0c972be33e1f5198fa96f400e3b76204bed57
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-20 15:45:58 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.cpp
@@ -1,32 +1,39 @@
 class Solution {
   public:
-    bool dfs(int i,vector<vector<int>> &arr,vector<int> &visit,vector<int> &path){
-        visit[i]=1;
-        path[i]=1;
-        for(auto it:arr[i]){
-            if(!visit[it]){
-                if(dfs(it,arr,visit,path)) return true;
-            }
-            else if(path[it]==1){
-                return true;
-            }
-        }
-        path[i]=0;
-        return false;
-    }
+    
     bool isCyclic(int V, vector<vector<int>> &edge) {
        vector<vector<int>> arr(V);
-       for(auto it:edge){
-           arr[it[0]].push_back(it[1]);
-       }
-       vector<int> visit(V,0);
-       vector<int> path(V,0);
-       for(int i=0;i<V;i++){
-           if(!visit[i]){
-               if(dfs(i,arr,visit,path)) return true;
-           }
-       }
-       return false;
+        vector<int> visit(V,0);
+        vector<int> result;
+        vector<int> in(V,0);
+        queue<int> q;
+        for(auto it:edge){
+            arr[it[0]].push_back(it[1]);
+            in[it[1]]++;
+        }
+        for(int i=0;i<V;i++){
+            if(in[i]==0){
+                q.push(i);
+                result.push_back(i);
+                visit[i]=1;
+            }
+        }
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            for(auto it:arr[front]){
+                if(!visit[it]){
+                    in[it]--;
+                    if(in[it]==0){
+                        result.push_back(it);
+                        q.push(it);
+                        visit[it]=1;
+                    }
+                    
+                }
+            }
+        }
+        return result.size()!=V;
     }
 };
 
