67548e39658def59ba2c49f43220c8ac398bfa21
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-21 16:58:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Geek's Training/geeks-training.cpp
+++ Difficulty: Medium/Geek's Training/geeks-training.cpp
@@ -1,62 +1,34 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int solve(int idx,int task,vector<vector<int>> &arr,vector<vector<int>> &dp){
-        if(idx==0){
+    int solve(int i,int task,vector<vector<int>>&arr,vector<vector<int>>&dp){
+        if(i==0){
             int maxi=0;
-            for(int i=0;i<=2;i++){
-                if(i!=task){
-                  maxi=max(maxi,arr[0][i]);  
+            for(int j=0;j<3;j++){
+                if(j!=task){
+                    maxi=max(maxi,arr[0][j]);
                 }
             }
-            return maxi;
+            return dp[0][task]=maxi;
         }
-        if(dp[idx][task]!=0)return dp[idx][task];
-        int ans=0;
-        for(int i=0;i<=2;i++){
-          if(i!=task){
-              int check=arr[idx][i]+solve(idx-1,i,arr,dp);
-              ans=max(ans,check);
-          }
+        if(dp[i][task]!=-1)return dp[i][task];
+        int merit=0;
+        for(int j=0;j<3;j++){
+            if(j!=task){
+                int point=arr[i][j]+solve(i-1,j,arr,dp);
+                merit=max(merit,point);
+            }
         }
-        return dp[idx][task]=ans;
+        return dp[i][task]=merit;
+        
     }
     int maximumPoints(vector<vector<int>>& arr) {
-      int n=arr.size();
-      vector<vector<int>> dp(n+1,vector<int> (4,0));
-      return solve(n-1,3,arr,dp);
+        // Code here
+        int n=arr.size();
+        // return solve(n-1,3,arr);
+        vector<vector<int>> dp(n+1,vector<int>(4,-1));
+        int a=solve(n-1,0,arr,dp);
+        int b=solve(n-1,1,arr,dp);
+        int c=solve(n-1,2,arr,dp);
+        return max({dp[n-1][1],dp[n-1][0],dp[n-1][2]});
     }
 };
-
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        vector<vector<int>> arr;
-        for (int i = 0; i < n; ++i) {
-            vector<int> temp;
-            for (int j = 0; j < 3; ++j) {
-                int x;
-                cin >> x;
-                temp.push_back(x);
-            }
-            arr.push_back(temp);
-        }
-
-        Solution obj;
-        cout << obj.maximumPoints(arr) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
