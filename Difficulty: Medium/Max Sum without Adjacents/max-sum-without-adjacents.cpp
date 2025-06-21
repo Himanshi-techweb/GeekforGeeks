3078481b120039712658575bd45df6e593b562e7
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-21 15:03:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
+++ Difficulty: Medium/Max Sum without Adjacents/max-sum-without-adjacents.cpp
@@ -1,54 +1,20 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function template for C++
 class Solution {
   public:
-    int solve(vector<int> & arr,vector<int> &dp,int idx){
-        if(idx>=arr.size())return 0; 
-        if(dp[idx]!=-1)return dp[idx];
-        int include=solve(arr,dp,idx+2)+arr[idx];
-        int exclude=solve(arr,dp,idx+1)+0;
-        int ans=max(include,exclude);
-        return dp[idx]=ans;
+    int solve(int i,vector<int> &arr,vector<int> &dp){
+        if(i==0)return arr[0];
+        if(dp[i]!=-1)return dp[i];
+        // int taken=INT_MIN;
+        int taken=arr[i]+solve(i-2,arr,dp);
+        int nottaken=solve(i-1,arr,dp);
+        return dp[i]=max(taken,nottaken);
     }
     // calculate the maximum sum with out adjacent
     int findMaxSum(vector<int>& arr) {
-        vector<int> dp(arr.size()+1,-1);
-        return solve(arr,dp,0);
+        // code here
+        int n=arr.size();
+        vector<int> dp(n+1,-1);
+        solve(n-1,arr,dp);
+        return dp[n-1];
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        int res = obj.findMaxSum(arr);
-        cout << res << endl;
-        cout << "~" << endl;
-        // string tl;
-        // getline(cin, tl);
-    }
-    return 0;
-}
-
-// } Driver Code Ends
