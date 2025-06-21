917e92212efe548d24bae476d01ef98a56d567ed
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-22 00:17:06 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.cpp
+++ Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.cpp
@@ -1,54 +1,24 @@
-//{ Driver Code Starts
-// Initial Template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // User function Template for C++
 
 class Solution {
   public:
-    int maximumPath(vector<vector<int>>& matrix) {
-      int n=matrix.size();
-      int m=matrix[0].size();
-      vector<int> next=matrix[n-1];
-      
-      for(int i=n-2;i>=0;i--){
-        vector<int> curr(m);
-        for(int j=0;j<m;j++){
-        int left=(j>0)?next[j-1]:INT_MIN;
-        int below=next[j];
-        int right=(j<m-1)?next[j+1]:INT_MIN;
-        curr[j]=matrix[i][j]+max(left,max(below,right));
+    int solve(int i,int j,vector<vector<int>> & mat,vector<vector<int>>&dp){
+        if(i<0 || j<0 || j>=mat[0].size())return INT_MIN;
+        if(i==0)return dp[0][j]=mat[0][j];
+        if(dp[i][j]!=-1)return dp[i][j];
+        int left=solve(i-1,j-1,mat,dp);
+        int just=solve(i-1,j,mat,dp);
+        int right=solve(i-1,j+1,mat,dp);
+        return dp[i][j]=mat[i][j]+max({left,just,right});
+    }
+    int maximumPath(vector<vector<int>>& mat) {
+        // code here
+        int m=mat.size();
+        int n=mat[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        for(int i=0;i<n;i++){
+            dp[m-1][i]=solve(m-1,i,mat,dp);
         }
-        next=curr;
-      }
-      return *max_element(next.begin(),next.end());
+        return *max_element(dp[m-1].begin(),dp[m-1].end());
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, m;
-        cin >> n >> m;
-        vector<vector<int>> mat(n, vector<int>(m));
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                cin >> mat[i][j];
-            }
-        }
-        Solution ob;
-        cout << ob.maximumPath(mat) << "\n";
-        cout << "~\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
