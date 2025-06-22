37aedc6607ef78030d081d1d7974f7e6a5de86ca
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-22 13:43:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -1,61 +1,17 @@
-//{ Driver Code Starts
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
-// User function template for C++
-
 class Solution {
   public:
-    bool solve(int i,int tar,vector<int> &arr,vector<vector<int>> &dp){
-        if(tar==0)return true;
-        if(i==0)return arr[i]==tar;
-        
-        if(dp[i][tar]!=-1)return dp[i][tar];
-        bool take=(tar>=arr[i])?solve(i-1,tar-arr[i],arr,dp):false;
-        bool nottake=solve(i-1,tar,arr,dp);
-        return dp[i][tar]=take || nottake;
+    bool check(int i,vector<int> &arr,int tar,vector<vector<int>> &dp){
+      if(tar==0)return true;
+      if(i<0 || tar<0)return false;
+      if(dp[i][tar]!=-1)return dp[i][tar];
+      bool taken=check(i-1,arr,tar-arr[i],dp);
+      bool nottaken=check(i-1,arr,tar,dp);
+      return dp[i][tar]=taken|| nottaken;
     }
-    bool isSubsetSum(vector<int>& arr, int target) {
+    bool isSubsetSum(vector<int>& arr, int sum) {
         // code here
-        int n=arr.size();
-        vector<vector<int>> dp(n,vector<int>(target+1,-1));
-        return solve(n-1,target,arr,dp);
+        int m=arr.size();
+        vector<vector<int>> dp(m,vector<int>(sum+1,-1));
+        return check(arr.size()-1,arr,sum,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int sum;
-        cin >> sum;
-        cin.ignore();
-
-        Solution ob;
-        if (ob.isSubsetSum(arr, sum))
-            cout << "true" << endl;
-        else
-            cout << "false" << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
