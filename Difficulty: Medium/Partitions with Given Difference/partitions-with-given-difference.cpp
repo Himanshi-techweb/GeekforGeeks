566a59a69c0d44ecf82db2fcb85e6f037e61b86b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-22 20:16:51 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
+++ Difficulty: Medium/Partitions with Given Difference/partitions-with-given-difference.cpp
@@ -1,79 +1,30 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int countPartitions(vector<int>& nums, int d) {
-        int n=nums.size();
-        int sum=0;
-        for(auto x:nums){
-            sum+=x;
+    int countPartitions(vector<int>& arr, int d) {
+        // Code here
+        int m=arr.size();
+        int sum=accumulate(arr.begin(),arr.end(),0);
+        int cnt=0;
+        int target=sum+d;
+        vector<vector<int>> dp(m,vector<int>(target+1,0));
+        for(int i=0;i<m;i++){
+            dp[i][0]=1;
         }
-        //sum1-sum2=d//sum1=sum-sum2//sum1=(total-d)/2;
-        //tar=(sum-d)/2;
-        if((sum-d)%2!=0 || sum<d)return 0;
-        int tar=(sum-d)/2;
-        vector<vector<int>> dp(n,vector<int> (tar+1,0));
-        if(nums[0]==0){
-            dp[0][0]=2;
+        
+        if(target%2!=0)return 0;
+        if(arr[0]<=target)dp[0][arr[0]]+=1;
+        for(int i=1;i<m;i++){
+            for(int j=0;j<=target;j++){
+                int taken=0;
+                if(arr[i]<=j)taken=dp[i-1][j-arr[i]];
+                int nottaken=dp[i-1][j];
+                dp[i][j]=taken + nottaken;
+            }
         }
-        else{
-          dp[0][0]=1;  
+        if(target%2==0){
+            target=target/2;
+                cnt+=dp[m-1][target];
         }
-        if(nums[0]!=0 && nums[0]<=tar){
-            dp[0][nums[0]]=1;
-        }
-        for(int i=1;i<n;i++){
-          for(int j=0;j<=tar;j++){
-              int nottake=dp[i-1][j];
-              int take=(nums[i]<=j)?dp[i-1][j-nums[i]]:0;
-              dp[i][j]=take+nottake;
-          }  
-        }
-        //cout<<dp[n-1][tar]<<endl;
-        return dp[n-1][tar];
+        return cnt;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.countPartitions(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
