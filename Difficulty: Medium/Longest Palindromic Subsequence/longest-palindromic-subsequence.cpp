7a160289506d67aabfb75382325542070739ddf5
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-23 17:57:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Longest Palindromic Subsequence/longest-palindromic-subsequence.cpp
@@ -0,0 +1,31 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    int longestPalinSubseq(string &x) {
+        // code here
+        string y=x;
+       reverse(y.begin(),y.end());
+       int m=x.size();
+       int n=y.size();
+       vector<vector<int>> dp(m+1,vector<int> (n+1,0));
+       vector<int> prev(n+1,0);
+        for(int i=0;i<=m;i++){
+            dp[i][0]=0;
+        } 
+        for(int j=0;j<=n;j++){
+            dp[0][j]=0;
+        } 
+        for(int i=1;i<=m;i++){
+          for(int j=1;j<=n;j++){
+            if(x[i-1]==y[j-1])dp[i][j]= 1+dp[i-1][j-1];
+            else{
+              int left=dp[i-1][j];
+              int right=dp[i][j-1];
+              dp[i][j]= max(left,right);
+            }
+          }
+        }
+        return dp[m][n];
+    }
+};
