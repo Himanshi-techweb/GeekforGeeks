49bd10e083f55e5b2898fa759d3c041ba0673915
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-28 11:48:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Postfix to Infix Conversion/postfix-to-infix-conversion.cpp
@@ -0,0 +1,34 @@
+// User function Template for C++
+
+class Solution {
+  public:
+    string postToInfix(string arr) {
+        // Write your code here
+        stack<string> st;
+        int i=0;
+        int n=arr.size();
+        while(i<n){
+          char x=arr[i];
+          if(isalnum(x)){
+              st.push(string(1,x));
+          }
+          else{
+              if(x=='*' || x=='/' || x=='-' || x=='+' || x=='^'){
+                  string op1="";string op2="";
+                  if(!st.empty()){
+                    op1=st.top();
+                    st.pop();
+                  } 
+                  if(!st.empty()){
+                    op2=st.top();
+                    st.pop();
+                  }
+                  string y="(" +op2+x+op1+")";
+                  st.push(y);
+              }
+          }
+          i++;
+        }
+        return st.top();
+    }
+};
