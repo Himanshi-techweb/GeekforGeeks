9e02943899bcdacc08dfc6de510bf36f1f026322
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-29 18:58:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
+++ Difficulty: Medium/0 - 1 Knapsack Problem/0-1-knapsack-problem.cpp
@@ -1,13 +1,14 @@
 class Solution {
   public:
     int solve(int i,int w,vector<int> &val,vector<int> &wt,vector<vector<int>>&dp){
-        if(i==0 ){
-            return (wt[0]<=w)?val[0]:0;
+        if(i==0){
+            if(wt[0]<=w)return val[0];
+            return 0;
         }
         if(dp[i][w]!=-1)return dp[i][w];
-        int take=INT_MIN;int nottake=INT_MIN;
-        if(w-wt[i]>=0) take=val[i]+solve(i-1,w-wt[i],val,wt,dp);
-        nottake=solve(i-1,w,val,wt,dp);
+        int take=0;
+        if(w>=wt[i])take= val[i]+solve(i-1,w-wt[i],val,wt,dp);
+        int nottake=solve(i-1,w,val,wt,dp);
         return dp[i][w]=max(take,nottake);
     }
     int knapsack(int W, vector<int> &val, vector<int> &wt) {
