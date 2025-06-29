4e5239d9e7cd8dae7916355522b9e9cae7f0e392
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-29 14:35:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.cpp
+++ Difficulty: Medium/Maximum path sum in matrix/maximum-path-sum-in-matrix.cpp
@@ -2,22 +2,25 @@
 
 class Solution {
   public:
-    int solve(int i,int j,vector<vector<int>> & mat,vector<vector<int>>&dp){
-        if(i<0 || j<0 || j>=mat[0].size())return INT_MIN;
-        if(i==0)return dp[0][j]=mat[0][j];
+    int solve(int i,int j,vector<vector<int>>& arr,vector<vector<int>> &dp){
+        if(i<0 || j<0 || j>=arr[0].size())return INT_MIN;
+        if(i==0)return arr[0][j];
         if(dp[i][j]!=-1)return dp[i][j];
-        int left=solve(i-1,j-1,mat,dp);
-        int just=solve(i-1,j,mat,dp);
-        int right=solve(i-1,j+1,mat,dp);
-        return dp[i][j]=mat[i][j]+max({left,just,right});
+        int left=INT_MIN;
+        int right=INT_MIN;
+        int above=INT_MIN;
+        left=solve(i-1,j-1,arr,dp);
+        right=solve(i-1,j+1,arr,dp);
+        above=solve(i-1,j,arr,dp);
+        return dp[i][j]=arr[i][j]+max({left,right,above});
     }
     int maximumPath(vector<vector<int>>& mat) {
         // code here
         int m=mat.size();
         int n=mat[0].size();
-        vector<vector<int>> dp(m,vector<int>(n,-1));
-        for(int i=0;i<n;i++){
-            dp[m-1][i]=solve(m-1,i,mat,dp);
+        vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+        for(int j=0;j<n;j++){
+            dp[m-1][j]=solve(m-1,j,mat,dp);
         }
         return *max_element(dp[m-1].begin(),dp[m-1].end());
     }
