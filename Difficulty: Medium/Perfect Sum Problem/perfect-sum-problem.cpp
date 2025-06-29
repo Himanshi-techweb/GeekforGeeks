860d9a080762efd0aaa3515060096ee7ea81bc52
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-29 18:37:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
+++ Difficulty: Medium/Perfect Sum Problem/perfect-sum-problem.cpp
@@ -1,66 +1,20 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
-    int perfectSum(vector<int>& nums, int tar) {
-     int n=nums.size();
-        // int tar=sum/2;
-        vector<vector<int>> dp(n,vector<int> (tar+1,false));
-        vector<int>prev(tar+1,0);
-        vector<int> curr(tar+1);
-        prev[0]=curr[0]=1;
-        //  return solve(n-1,tar,nums,dp);
-        // for(int j=0;j<n;j++){
-        //   prev[0]=1;
-        // }
-        if(nums[0]<=tar)prev[nums[0]]+=1;
-        
-        for(int i=1;i<n;i++){
-            for(int j=0;j<=tar;j++){
-                int nottake=prev[j];
-                int take=(j>=nums[i])?prev[j-nums[i]]:0;
-                 curr[j]=nottake + take; 
-            }prev=curr;
+    int check(int i,int sum,vector<int> &arr,vector<vector<int>> &dp){
+        if(i==0){
+           if(arr[0]==sum && sum==0)return 2;
+           else if(sum==0 || arr[0]==sum )return 1;
+           else return 0;
         }
-        
-        return prev[tar];
+        if(dp[i][sum]!=-1)return dp[i][sum];
+        int taken=0;
+        if(sum>=arr[i])taken=check(i-1,sum-arr[i],arr,dp);
+        int nottaken=check(i-1,sum,arr,dp);
+        return dp[i][sum]=taken+nottaken;
+    }
+    int perfectSum(vector<int>& arr, int sum) {
+        // code here
+        vector<vector<int>> dp(arr.size(),vector<int> (sum+1,-1)); 
+        return check(arr.size()-1,sum,arr,dp);
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // Ignore newline character after t
-
-    while (t--) {
-        vector<int> arr;
-        int target;
-        string inputLine;
-
-        getline(cin, inputLine); // Read the array input as a line
-        stringstream ss(inputLine);
-        int value;
-        while (ss >> value) {
-            arr.push_back(value);
-        }
-
-        cin >> target;
-        cin.ignore(); // Ignore newline character after target input
-
-        Solution solution;
-        cout << solution.perfectSum(arr, target);
-        cout << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
