78f89e6bcd133dc0e86f2d1a8901433010aca6da
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-06-30 23:45:19 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
+++ Difficulty: Medium/Fractional Knapsack/fractional-knapsack.cpp
@@ -1,10 +1,3 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 // class implemented
 /*
 struct Item{
@@ -12,79 +5,30 @@ struct Item{
     int weight;
 };
 */
-//solve it by greedy approach;
-class comparator{
-  public:
-  bool operator()(pair<int,int> a,pair<int,int> b){
-     return (double)a.first/a.second>(double)b.first/b.second;
-  }
-};
+
 class Solution {
   public:
-    // Function to get the maximum total value in the knapsack.
-    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int capacity) {
-        vector<pair<int,int>> item;
+    double fractionalKnapsack(vector<int>& val, vector<int>& wt, int c) {
+        // code here
+        vector<vector<int>> check;
         for(int i=0;i<wt.size();i++){
-            item.push_back({val[i],wt[i]});
+            check.push_back({val[i],wt[i]});
         }
-        sort(item.begin(),item.end(),comparator());
+        sort(check.begin(),check.end(),[](const auto &a,const auto &b){
+            // return (double)a[0]/a[1]>(double)b[0]/b[1];
+            return (double)a[0]/a[1]>(double)b[0]/b[1];
+        });
         double total=0;
-        for(int i=0;i<wt.size();i++){
-            if(item[i].second<=capacity){
-                capacity=capacity-item[i].second;
-                total+=item[i].first;
+        for(int i=0;i<check.size();i++){
+            if(c>=check[i][1]){
+                c=c-check[i][1];
+                total+=check[i][0];
             }
             else{
-                total+=(((double)item[i].first/item[i].second)*capacity);
+                total+=(((double)c/check[i][1])*check[i][0]);
                 break;
             }
         }
         return total;
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    // taking testcases
-    cin >> t;
-    cin.ignore(); // to ignore the newline after the number of test cases
-    cout << setprecision(6) << fixed;
-
-    while (t--) {
-        // Reading the value array
-        vector<int> values;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            values.push_back(number);
-        }
-
-        // Reading the weight array
-        vector<int> weights;
-        getline(cin, input);
-        stringstream ss2(input);
-        while (ss2 >> number) {
-            weights.push_back(number);
-        }
-
-        // Reading the capacity
-        int w;
-        cin >> w;
-        cin.ignore(); // to ignore the newline after capacity
-
-        // function call
-        Solution ob;
-        cout << ob.fractionalKnapsack(values, weights, w) << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
