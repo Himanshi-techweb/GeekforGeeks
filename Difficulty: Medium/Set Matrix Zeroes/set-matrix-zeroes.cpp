04c939999b03809ae1a4173fb0e598a6c7c52eb0
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-02 16:35:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.cpp
+++ Difficulty: Medium/Set Matrix Zeroes/set-matrix-zeroes.cpp
@@ -3,34 +3,44 @@ class Solution {
   public:
     void setMatrixZeroes(vector<vector<int>> &arr) {
         // code here
-        bool frow=false;bool fcol=false;
+        bool f_row=0;bool f_col=0;
         int m=arr.size();int n=arr[0].size();
         for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(arr[i][j]==0){
-                    arr[0][j]=0;
-                    arr[i][0]=0;
-                    if(i==0)frow=true;
-                    if(j==0)fcol=true;
-                }
-                
-            }
+            if(arr[i][0]==0)f_col=true;
+        }
+        for(int i=0;i<n;i++){
+            if(arr[0][i]==0)f_row=true;
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                if(arr[i][0]==0 || arr[0][j]==0){
+               if(arr[i][j]==0){
+                   arr[i][0]=0;
+                   arr[0][j]=0;
+               } 
+            }
+        }
+        for(int i=1;i<m;i++){
+            if(arr[i][0]==0){
+                for(int j=0;j<n;j++){
                     arr[i][j]=0;
                 }
             }
         }
-        if(frow){
-            for(int j=0;j<n;j++){
-                arr[0][j]=0;
+        for(int i=1;i<n;i++){
+            if(arr[0][i]==0){
+                for(int j=0;j<m;j++){
+                    arr[j][i]=0;
+                }
             }
         }
-        if(fcol){
+        if(f_col){
             for(int i=0;i<m;i++){
                 arr[i][0]=0;
+            }
+        }
+        if(f_row){
+            for(int i=0;i<n;i++){
+                arr[0][i]=0;
             }
         }
     }
