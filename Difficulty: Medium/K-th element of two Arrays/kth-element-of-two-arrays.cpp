ec848129e85d405aef74afec7f6f223f2e8d6a2a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-04 22:18:10 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -1,65 +1,28 @@
 class Solution {
   public:
     int kthElement(vector<int>& a, vector<int>& b, int k) {
-        // int n1=a.size();int n2=b.size();
-        // // code here
-        // int cnt=0;
-        // int i=0;int j=0;
-        // while(i<n1 && j<n2){
-        //     if(a[i]<b[j]){
-        //         cnt++;
-        //         if(cnt==k)return a[i];
-        //         i++;
-        //     }
-        //     else{
-        //       cnt++;
-        //       if(cnt==k)return b[j];
-        //       j++;
-        //     }
-        // }
-        // while(i<n1){
-        //         cnt++;
-        //         if(cnt==k)return a[i];
-        //         i++;
-        // }
-        // while(j<n2){
-        //       cnt++;
-        //       if(cnt==k)return b[j];
-        //       j++;
-        // }
-        // return 0;
-        
-        
-        
-        
-        
-        int x1=a.size();
-        int x2=b.size();
-        if(x1>x2)return kthElement(b,a,k);
-        int low=max(0,k-x2);
-        int high=min(k,x1);
+        int n1=a.size();
+        int n2=b.size();
+        if(n1>n2)return kthElement(b,a,k);
+        int n=n1+n2;
         int left=k;
+        int low=max(0,k-n2);
+        int high=min(k,n1);
+        int l1=INT_MIN;int l2=INT_MIN;int r1=INT_MAX;int r2=INT_MAX;
         while(low<=high){
             int mid1=low+(high-low)/2;
             int mid2=left-mid1;
-            int l1=INT_MIN;int l2=INT_MIN;
-            int r1=INT_MAX;int r2=INT_MAX;
-            if(mid1>0)l1=a[mid1-1];
-            if(mid2>0)l2=b[mid2-1];
-            if(mid1<x1)r1=a[mid1];
-            if(mid2<x2)r2=b[mid2];
+            int l1 = (mid1 > 0) ? a[mid1 - 1] : INT_MIN;
+            int l2 = (mid2 > 0) ? b[mid2 - 1] : INT_MIN;
+            int r1 = (mid1 < n1) ? a[mid1] : INT_MAX;
+            int r2 = (mid2 < n2) ? b[mid2] : INT_MAX;
             if(l1<=r2 && l2<=r1){
-                if((x1+x2)%2==0){
-                    return max(l1,l2);
-                }
-                else{
-                    return max(l1,l2);
-                }
+                return max(l1,l2);
             }
             else if(l1>r2){
                 high=mid1-1;
             }
-            else{
+            else if(l2>r1){
                 low=mid1+1;
             }
         }
