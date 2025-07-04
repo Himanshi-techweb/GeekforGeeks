154115413f30374f12924209c9411108a33c985b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-04 19:49:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Smallest Divisor/smallest-divisor.cpp
+++ Difficulty: Medium/Smallest Divisor/smallest-divisor.cpp
@@ -1,9 +1,15 @@
 class Solution {
   public:
-    bool check(vector<int> &arr,int mid,int k){
+    bool check(int mid,vector<int> &arr,int k){
         int cnt=0;
-        for(auto x:arr){
-            cnt+=(ceil((double)x/mid));
+        for(auto it:arr){
+            if(it<mid)cnt++;
+            else if(it%mid==0){
+                cnt+=(it/mid);
+            }
+            else {
+                cnt=cnt+(it/mid)+1;
+            }
         }
         return cnt<=k;
     }
@@ -11,17 +17,17 @@ class Solution {
         // Code here
         int low=1;
         int high=*max_element(arr.begin(),arr.end());
-        int ans=0;
+        int divi=1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(check(arr,mid,k)){
-                ans=mid;
+            if(check(mid,arr,k)){
+                divi=mid;
                 high=mid-1;
             }
             else{
                 low=mid+1;
             }
         }
-        return ans;
+        return divi;
     }
 };
