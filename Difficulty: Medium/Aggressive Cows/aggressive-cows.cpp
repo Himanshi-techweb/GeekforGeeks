73f5a25670d0c939d748c87011265dbb7c856cdb
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-04 20:19:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
+++ Difficulty: Medium/Aggressive Cows/aggressive-cows.cpp
@@ -2,32 +2,34 @@
 
 class Solution {
   public:
-    bool ischeck(int mid,int cows,vector<int> &arr){
-        int cntcow=1;int last=arr[0];
-        for(auto x:arr){
-            if(x-last>=mid){
-                cntcow++;
-                last=x;
+    bool check(int mid,vector<int> &arr,int k){
+        int cnt=1;int prev=arr[0];
+        for(int i=1;i<arr.size();i++){
+            if(arr[i]-prev>=mid){
+                cnt++;
+                prev=arr[i];
             }
-            if(cntcow>=cows)return true;
-        }return false;
+            if(cnt>=k)return true;
+        }
+        return false;
     }
     int aggressiveCows(vector<int> &arr, int k) {
 
         // Write your code here
         sort(arr.begin(),arr.end());
-        int maxi=arr[arr.size()-1]-arr[0];
-        int low=1;int high=maxi;int ans=0;
+        int low=1;
+        int high=arr[arr.size()-1]-arr[0];
+        int dist=1;
         while(low<=high){
-            int mid=low + (high-low)/2;
-            if(ischeck(mid,k,arr)){
-                ans=mid;
+            int mid=low+(high-low)/2;
+            if(check(mid,arr,k)){
+                dist=mid;
                 low=mid+1;
             }
             else{
                 high=mid-1;
             }
         }
-        return ans;
+        return dist;
     }
 };
