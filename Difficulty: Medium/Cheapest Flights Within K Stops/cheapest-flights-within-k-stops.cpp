554dabe3d78b824488e60ac6486fc89ed1c3881d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-05 10:55:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Cheapest Flights Within K Stops/cheapest-flights-within-k-stops.cpp
+++ Difficulty: Medium/Cheapest Flights Within K Stops/cheapest-flights-within-k-stops.cpp
@@ -2,31 +2,29 @@ class Solution {
   public:
     int CheapestFLight(int n, vector<vector<int>>& flight, int src, int dst, int k) {
         // Code here
-        queue<pair<int,pair<int,int>>> q;
-        q.push({0,{src,0}});//stop,node,distance
-        vector<int> dist(n,INT_MAX);
-        vector<vector<pair<int,int>>> arr(n);
-        for(int i=0;i<flight.size();i++){
-            arr[flight[i][0]].push_back({flight[i][1],flight[i][2]});
+        
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto it:flight){
+            int u=it[0];int v=it[1];int price=it[2];
+            adj[u].push_back({v,price});
         }
+        queue<pair<int,pair<int,int>>> q;//(stop,(price,node))
+        vector<int> dist(n,INT_MAX);
         dist[src]=0;
+        q.push({0,{0,src}});
         while(!q.empty()){
             int stop=q.front().first;
-            int node=q.front().second.first;
-            int price=q.front().second.second;
+            int price=q.front().second.first;
+            int node=q.front().second.second;
             q.pop();
-            if(stop>k)continue;
-            for(auto it:arr[node]){
-                int adjprice=it.second;
-                int adjnode=it.first;
-                 if((dist[adjnode]>adjprice+price && adjnode==dst && stop<=k ) || (dist[adjnode]>adjprice+price && stop+1<=k && adjnode!=dst)){
-                   dist[adjnode]=adjprice+price; 
-                   q.push({stop+1,{adjnode,adjprice+price}});
-                }
+            if(stop>k+1)continue;
+            for(auto it:adj[node]){
+              if(dist[it.first]>price+it.second && stop<=k){
+                  dist[it.first]=price+it.second;
+                  q.push({stop+1,{dist[it.first],it.first}});
+              }  
             }
         }
-        if(dist[dst]==INT_MAX) return -1;
-        return dist[dst];
-        
+        return (dist[dst]!=INT_MAX)?dist[dst]:-1;
     }
 };
