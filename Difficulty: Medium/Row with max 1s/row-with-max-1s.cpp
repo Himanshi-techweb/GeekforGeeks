9293d576fd095c756b8f94a7f95d8ffec51d4500
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-10 16:34:12 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
+++ Difficulty: Medium/Row with max 1s/row-with-max-1s.cpp
@@ -3,21 +3,14 @@ class Solution {
   public:
     int rowWithMax1s(vector<vector<int>> &mat) {
         int m=mat[0].size();int max_one=0;int ans=-1;
-        vector<int> check(mat.size());
+        //vector<int> check(mat.size());
         for(int i=0;i<mat.size();i++){
-            //sort(mat[i].begin(),mat[i].end());
             int lb=lower_bound(mat[i].begin(),mat[i].end(),1)-mat[i].begin();
-            int one=m-lb;
-            if(one>max_one){
-                max_one=one;
+            if(m-lb>max_one){
+                max_one=m-lb;
                 ans=i;
             }
         }
-        // for(int i=0;i<check.size();i++){
-        //   if(ans<check[i]){
-        //     ans=i;
-        //   }
-        // }
         return ans;
     }
 };
