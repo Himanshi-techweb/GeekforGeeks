18df562fe31d2d09fd6132af93af86771f1fb933
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-15 13:34:18 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Check for BST/check-for-bst.cpp
+++ Difficulty: Easy/Check for BST/check-for-bst.cpp
@@ -3,12 +3,9 @@ class Solution {
     // Function to check whether a Binary Tree is BST or not.
     bool check(Node* root,int mini,int maxi){
         if(root==NULL)return true;
-        if(root->left==NULL && root->right==NULL)return true;
-        if(!check(root->left,mini,root->data) || !check(root->right,root->data,maxi))return false;
-        if(root->left && (root->left->data <=mini || root->left->data>=root->data))return false;
-        if(root->right && (root->right->data >=maxi || root->right->data<=root->data))return false;
+        if(!check(root->left,mini,root->data)|| !check(root->right,root->data,maxi)) return false;
+        if((root->data>maxi) || (root->data<mini))return false;
         return true;
-
     }
     bool isBST(Node* root) {
        return check(root,INT_MIN,INT_MAX);
