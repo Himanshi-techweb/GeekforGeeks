0b198608cd1b386bea2d8f69be57ea1fa63ef74b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-21 13:00:04 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
+++ Difficulty: Medium/Rat in a Maze Problem - I/rat-in-a-maze-problem-i.cpp
@@ -1,32 +1,33 @@
 class Solution {
   public:
-    vector<pair<pair<int,int>,int>> rc={{{-1,0},'U'},{{1,0},'D'},{{0,-1},'L'},{{0,1},'R'}};
     vector<string> ans;
-    void solve(int i,int j,vector<vector<int>> &arr,string &x,int n,vector<vector<int>> &visit){
-    if(i==n-1 && j==n-1){
-        ans.push_back(x);return;
-    }
-    if(i>=n || j>=n)return;
-    for(auto it:rc){
-        int nr=i+it.first.first;int nc=j+it.first.second;
-        if(nr>=0 && nr<n && nc>=0 && nc<n &&arr[nr][nc]==1 && !visit[nr][nc]){
-           x+=it.second;
-           visit[nr][nc]=1;
-           solve(nr,nc,arr,x,n,visit);
-           x.pop_back();
-           visit[nr][nc]=0;
+    vector<pair<pair<int,int>,char>> rc={{{0,-1},'L'},{{-1,0},'U'},{{0,1},'R'},{{1,0},'D'}};
+    void solve(int i,int j,vector<vector<int>> &arr,vector<vector<int>> &visit,int m,int n,string &curr){
+        if(i==m-1 && j==n-1){
+            ans.push_back(curr);
+            return;
         }
-    }
+        for(auto it:rc){
+            int nr=it.first.first+i;int nc=it.first.second+j;
+            if(nr>=0 && nr<m && nc>=0 && nc<n && !visit[nr][nc] && arr[nr][nc]==1){
+                visit[nr][nc]=1;
+                curr=curr+it.second;
+                solve(nr,nc,arr,visit,m,n,curr);
+                curr.pop_back();
+                visit[nr][nc]=0;
+            }
+        }
     }
     vector<string> ratInMaze(vector<vector<int>>& maze) {
     // Write your code here.
-    string x="";
-    int n=maze.size();
-    vector<vector<int>> visit(n,vector<int> (n,0));
-    if(maze[0][0]=='0' || maze[n-1][n-1]=='0')return {};
-    visit[0][0]=1;
-    solve(0,0,maze,x,n,visit);
-    sort(ans.begin(),ans.end());
-    return ans;
+       int m=maze.size();int n=maze[0].size();
+       if(maze[0][0]==0 || maze[m-1][n-1]==0)return {};
+       
+       string curr="";
+       vector<vector<int>> visit(m,vector<int> (n,0));
+       visit[0][0]=1;
+       solve(0,0,maze,visit,m,n,curr);
+       sort(ans.begin(),ans.end());
+       return ans;
     }
 };
