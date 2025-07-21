2d2a1c84c71887e9563d8b8255940d7017ad4903
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-07-21 12:09:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,31 +1,40 @@
 class Solution {
   public:
     //bool condition=false;bool condition2=false;
-    bool dfs(int src,int parent,vector<vector<int>>&adj,vector<int> &visit){
-       visit[src]=1;
-       for(auto it:adj[src]){
-           if(visit[it]!=1 && it!=parent){
-              if(dfs(it,src,adj,visit))return true; 
-           }
-           else if(visit[it]==1 && it!=parent){
-               return true;
-           }
-       }
-       return false;
-    }
+    
     bool isCycle(int V, vector<vector<int>>& edge) {
         // Code here
        vector<vector<int>> adj(V);
        for(auto it:edge){
            adj[it[0]].push_back(it[1]);
-            adj[it[1]].push_back(it[0]);
+           adj[it[1]].push_back(it[0]);
        }
+       queue<int> q;//node,parent
        vector<int> visit(V,0);
+       vector<int> parent(V,INT_MIN);
+       
        for(int i=0;i<V;i++){
            if(!visit[i]){
-               if(dfs(i,-1,adj,visit)) return true;
+               q.push(i);
+               visit[i]=1;
+               parent[i]=-1;
+               while(!q.empty()){
+                   int front=q.front();
+                   q.pop();
+                   for(auto it:adj[front]){
+                       if(!visit[it]){
+                           parent[it]=front;
+                           q.push(it);
+                           visit[it]=1;
+                       }
+                       else if(visit[it] && parent[it]!=-1 && parent[it]!=INT_MIN && it!=parent[front]){
+                           return true;
+                       }
+                   }
+               }
            }
        }
        return false;
+       
     }
 };
