f07e86c0f1aa17ffd30578a58d75e7fbbd10f4a3
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2025-08-01 19:57:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -4,37 +4,31 @@ class Solution {
     
     bool isCycle(int V, vector<vector<int>>& edge) {
         // Code here
-       vector<vector<int>> adj(V);
+       vector<vector<int>> adj(V+1);
        for(auto it:edge){
            adj[it[0]].push_back(it[1]);
            adj[it[1]].push_back(it[0]);
        }
-       queue<int> q;//node,parent
-       vector<int> visit(V,0);
-       vector<int> parent(V,INT_MIN);
-       
-       for(int i=0;i<V;i++){
+       vector<int> visit(V+1);
+       for(int i=0;i<=V;i++){
+           queue<pair<int,int>> q;
+        q.push({i,-1});//node,parent
            if(!visit[i]){
-               q.push(i);
-               visit[i]=1;
-               parent[i]=-1;
-               while(!q.empty()){
-                   int front=q.front();
-                   q.pop();
-                   for(auto it:adj[front]){
-                       if(!visit[it]){
-                           parent[it]=front;
-                           q.push(it);
-                           visit[it]=1;
-                       }
-                       else if(visit[it] && parent[it]!=-1 && parent[it]!=INT_MIN && it!=parent[front]){
-                           return true;
-                       }
-                   }
-               }
+            visit[i]=1;
+            while(!q.empty()){
+           int node=q.front().first;int parent=q.front().second;
+           q.pop();
+           for(auto it:adj[node]){
+              if(!visit[it]){
+                  visit[it]=1;
+                  q.push({it,node});
+              }
+              else if(parent!=-1 && it!=parent)return true;
+           }
+       }   
            }
        }
-       return false;
        
+       return false;
     }
 };
