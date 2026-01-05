87ed000a0f8c6dd8f4deb394614fa5d37f7d4493
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-01-05 23:12:27 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
+++ Difficulty: Medium/Kadane's Algorithm/kadanes-algorithm.cpp
@@ -4,10 +4,10 @@ class Solution {
         // Code here
         int maxi=INT_MIN;
         int sum=0;
-        for(auto it:arr){
-          sum+=it;
-          maxi=max(maxi,sum);
-          if(sum<0)sum=0;
+        for(int i=0;i<arr.size();i++){
+            sum+=arr[i];
+            maxi=max(maxi,sum);
+            if(sum<0)sum=0;
         }
         return maxi;
     }
