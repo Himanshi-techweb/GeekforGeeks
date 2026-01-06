06dd3d10506fd22944cc397d53f36fe7f3e70dfc
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-01-06 16:43:29 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -2,18 +2,18 @@ class Solution {
   public:
     int minJumps(vector<int>& arr) {
         // code here
-        if(arr[0]==0)return -1;
-        vector<int> check;
-        int i=0;int cnt=0;int j=0;
-        while(j<arr.size()-1){
-          int maxreach=0;
-          for(int k=i;k<=j;k++){
-              maxreach=max(maxreach,k+arr[k]);
-          }
-          i=j+1;
-          j=maxreach;
-          if(i>j)return -1;
-          cnt++;
+        int l=0;
+        int r=0;
+        int cnt=0;
+        while(r<arr.size()-1){
+            int farthest=r;
+            for(int i=l;i<=r;i++){
+                farthest=max(farthest,arr[i]+i);
+            }
+            if(farthest==r)return -1;
+            cnt++;
+            l=r+1;
+            r=farthest;
         }
         return cnt;
     }
