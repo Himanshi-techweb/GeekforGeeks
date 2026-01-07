88865369a9601cb5700ef6ed306672f467205e7f
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-01-07 18:32:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.cpp
@@ -1,27 +1,28 @@
 class Solution {
   public:
-    vector<int> intersection(vector<int> &a,vector<int>&b){
-        vector<int> ans;
-        int i=0;int j=0;
-        while(i<a.size() && j<b.size()){
-            if(a[i]==b[j]){
-                ans.push_back(a[i]);
-                int k=a[i];
-                while(i<a.size() && a[i]==k)i++;
-                while(j<b.size() && b[j]==k)j++;
-            }
-            else if(a[i]<b[j])i++;
-            else j++;
-        }
-        return ans;
-    }
     // Function to find common elements in three arrays.
     vector<int> commonElements(vector<int> &arr1, vector<int> &arr2,
                                vector<int> &arr3) {
         // Code Here
-        vector<int> arr=intersection(arr1,arr2);
-        vector<int> ans=intersection(arr3,arr);
-        if(ans.size()==0)return {-1};
-        else return ans;
+        int i=0;int j=0;int k=0;
+        vector<int> ans;
+        int a=arr1.size();int b=arr2.size();int c=arr3.size();
+        while(i<a && j<b && k<c){
+            if(arr1[i]==arr2[j] && arr1[i]==arr3[k]){
+              int x=arr1[i];
+              ans.push_back(x);
+              while(i<a && arr1[i]==x)i++;
+              while(j<b && arr2[j]==x)j++;
+              while(k<c && arr3[k]==x)k++;
+            }
+            else{
+                int mini=min({arr1[i],arr2[j],arr3[k]});
+                if(arr1[i]==mini)i++;
+                if(arr2[j]==mini)j++;
+                if(arr3[k]==mini)k++;
+                
+            }
+        }
+        return ans;
     }
 };
