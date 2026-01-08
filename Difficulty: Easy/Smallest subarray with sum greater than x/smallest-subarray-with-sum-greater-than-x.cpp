eab68057b02fc361c846a56e8e46381310da4b4a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-01-08 23:02:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.cpp
+++ Difficulty: Easy/Smallest subarray with sum greater than x/smallest-subarray-with-sum-greater-than-x.cpp
@@ -2,18 +2,14 @@ class Solution {
   public:
     int smallestSubWithSum(int x, vector<int>& arr) {
         // Your code goes here
-        int sum=0;int i=0;
-        map<int,int> check;int mini=INT_MAX;
-        for(int i=0;i<arr.size();i++){
-            sum+=arr[i];
-            check[sum]=i;
-            if(sum>x){
-              auto it=check.lower_bound(sum-x);
-              if(it!=check.end()){
-                  mini=min(mini,i-it->second+1);
-              }
-            }
-            
+        int l=0;int r=0;int y=0;int mini=INT_MAX;
+        while(r<arr.size()){
+           y+=arr[r];
+           while(y>x && l<arr.size()){
+               mini=min(r-l+1,mini);
+               y-=arr[l];l++;
+           }
+           r++;
         }
         return mini==INT_MAX?0:mini;
     }
