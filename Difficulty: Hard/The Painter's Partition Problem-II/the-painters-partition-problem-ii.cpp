b8226803d96956676c93d4c2f9d080d04b6c8781
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-02-06 01:05:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
+++ Difficulty: Hard/The Painter's Partition Problem-II/the-painters-partition-problem-ii.cpp
@@ -1,36 +1,30 @@
-// User function template for C++
-
 class Solution {
   public:
-    bool check(int mid,vector<int>&arr,int k){
-        int cnt=1;int sum=0;
-        for(auto it:arr){
-           if(it>mid)return false;
-           if(sum+it<=mid){
-               sum=sum+it;
-           } 
-           else{
-               cnt++;
-               sum=it;
-           }
+    bool check(int mid,int k,vector<int> &arr){
+        int count=0;int sum=0;
+        for(int i=0;i<arr.size();i++){
+            if(sum+arr[i]>mid){
+                count++;
+                sum=arr[i];
+            }
+            else sum=sum+arr[i];
         }
-        return cnt<=k;
+        count++;
+        return count<=k;
     }
     int minTime(vector<int>& arr, int k) {
-      int low=*min_element(arr.begin(),arr.end());
-      int high=accumulate(arr.begin(),arr.end(),0);
-      int t=high;
-      if(k==1)return high;
-      while(low<=high){
-          int mid=low+(high-low)/2;
-          if(check(mid,arr,k)){
-              t=mid;
-              high=mid-1;
-          }
-          else{
-              low=mid+1;
-          }
-      }
-      return t;
+        // code here
+        int low=*max_element(arr.begin(),arr.end());
+        int high=accumulate(arr.begin(),arr.end(),0);
+        int ans=-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(check(mid,k,arr)){
+                ans=mid;
+                high=mid-1;
+            }
+            else low=mid+1;
+        }
+        return ans;
     }
 };
