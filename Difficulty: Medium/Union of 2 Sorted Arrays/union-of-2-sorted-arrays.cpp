6451e992c2fa3478684efd418a8067c28930b05a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-05 17:31:54 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -1,37 +1,40 @@
 class Solution {
   public:
-    // a,b : the arrays
-    // Function to return a list containing the union of the two arrays.
     vector<int> findUnion(vector<int> &a, vector<int> &b) {
-        // Your code here
-       vector<int> ans;
-       int i=0;int j=0;
-       while(i<a.size() && j<b.size()){
-           if(a[i]<=b[j]){
-              if(ans.size()==0 || ans.back()!=a[i]){
-                ans.push_back(a[i]);  
-              }
-              i++;
-           }
-           else{
-               if(ans.size()==0 ||ans.back()!=b[j]){
-                ans.push_back(b[j]);   
-               }
-               j++;
-           }
-       }
-       while(i<a.size()){
-           if(ans.back()!=a[i]){
-               ans.push_back(a[i]);
-           }
-           i++;
-       }
+        // code here
+        vector<int> ans;
+        int i=0;int j=0;
+        while(i<a.size() && j<b.size()){
+            
+            
+                if(a[i]<b[j]){
+                ans.push_back(a[i]);
+                i++;
+                }
+                else if(a[i]>b[j]){
+                ans.push_back(b[j]);
+                j++;
+                }
+                else if(a[i]==b[j]){
+                ans.push_back(a[i]);
+                i++;
+                j++;
+                }
+            while(ans.size()>0 && ans.back()==a[i])i++;
+            while(ans.size()>0 && ans.back()==b[j])j++;
+                
+            
+        }
+        while(i<a.size()){
+            ans.push_back(a[i]);
+            i++;
+            while(ans.back()==a[i])i++;
+        }
         while(j<b.size()){
-           if(ans.back()!=b[j]){
-               ans.push_back(b[j]);
-           }
-           j++;
-       }
-       return ans; 
+            ans.push_back(b[j]);
+            j++;
+            while(ans.back()==b[j])j++;
+        }
+        return ans;
     }
 };
