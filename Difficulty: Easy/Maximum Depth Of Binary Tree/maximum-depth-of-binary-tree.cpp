cd5bfbe40388febdbed5003e0c1d5aa6b128ade5
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-12 20:42:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Maximum Depth Of Binary Tree/maximum-depth-of-binary-tree.cpp
+++ Difficulty: Easy/Maximum Depth Of Binary Tree/maximum-depth-of-binary-tree.cpp
@@ -1,20 +1,24 @@
 /* The Node structure is
-struct Node
-{
+class Node {
+public:
     int data;
     Node* left;
     Node* right;
+
+};
 }; */
 
 class Solution {
   public:
-  
-    /*You are required to complete this method*/
-    int maxDepth(Node *root) {
-        // Your code here
+    int solve(Node* root){
         if(root==NULL)return 0;
-        int left=maxDepth(root->left);
-        int right=maxDepth(root->right);
+        int left=solve(root->left);
+        int right=solve(root->right);
         return 1+max(left,right);
+    }
+    int maxDepth(Node *root) {
+        // code here
+        return solve(root);
+        
     }
 };
