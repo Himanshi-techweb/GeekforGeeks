a609d74ccc48e034e804e7bd66e14209a4241d4a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-12 19:05:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
+++ Difficulty: Medium/Iterative Inorder/iterative-inorder.cpp
@@ -1,40 +1,37 @@
-// User function Template for C++
-
-/* Tree Node
-struct Node {
+/*
+Definition for Node
+class Node {
+  public:
     int data;
     Node* left;
     Node* right;
-};*/
+
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
+    }
+};
+*/
 class Solution {
   public:
-    //void morris(Node* root,)
     vector<int> inOrder(Node* root) {
         // code here
         if(root==NULL)return {};
-        vector<int> in;
+        stack<Node*> st;
         Node* curr=root;
-        while(curr){
-            if(!curr->left){
-                in.push_back(curr->data);
-                curr=curr->right;
+        vector<int> ans;
+        
+        while(curr || !st.empty()){
+            while(curr){
+                st.push(curr);
+                curr=curr->left;
             }
-            else{
-                Node* prev=curr->left;
-                while(prev->right && prev->right!=curr){
-                    prev=prev->right;
-                }
-                if(prev->right){
-                    curr->left=NULL;
-                    in.push_back(curr->data);
-                    curr=curr->right;
-                }
-                else{
-                   prev->right=curr;
-                   curr=curr->left;
-                }
-            }
+            curr=st.top();st.pop();
+            ans.push_back(curr->data);
+            curr=curr->right;
+            
+            
         }
-        return in;
+        return ans;
     }
 };
