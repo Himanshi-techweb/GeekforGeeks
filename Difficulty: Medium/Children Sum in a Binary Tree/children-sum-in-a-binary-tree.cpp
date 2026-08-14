d3a470eb7722adb1e61bd5c4661f8c434f0db631
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-14 15:29:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
+++ Difficulty: Medium/Children Sum in a Binary Tree/children-sum-in-a-binary-tree.cpp
@@ -1,38 +1,30 @@
-/*Complete the function below
-
-struct Node
-{
+/* Structure of a Tree Node
+class Node {
+public:
     int data;
-    struct Node* left;
-    struct Node* right;
-
-    Node(int x){80
-        data = x;
-        left = right = NULL;
+    Node* left, *right;
+    Node(int val) {
+        data = val;
+        left = right = nullptr;
     }
-};
-*/
+}; */
 
 class Solution {
   public:
-    // Function to check whether all nodes of a tree have the value
-    // equal to the sum of their child nodes.
-    bool check(Node* root){
-        if(root==NULL)return true;
-        if(root->left==NULL && root->right==NULL)return true;
-        int left=0;
-        if(root->left )left=root->left->data;
-        // else if(root->left && root->left->left) left=2*root->left->data;
-        int right=0;
-        if(root->right )right=root->right->data;
-        // else if(root->right && root->right->right) right=2*root->right->data;
+    pair<int,bool> solve(Node* root){
+        if(root==NULL)return {0,true};
+        if(root->left==NULL && root->right==NULL)return {root->data,true};
+        pair<int,bool> l=solve(root->left);
+        pair<int,bool> r=solve(root->right);
+        bool ans=((l.first+r.first)==root->data)&&l.second&&r.second;
+        return {root->data,ans};
         
-        bool condi=(root->data==left+right);
-        return check(root->left)&& check(root->right) && condi;
+        
     }
-    int isSumProperty(Node *root) {
-        // Add your code here
-        if(!check(root))return false;
-        return true;
+    bool isSumProperty(Node *root) {
+        // code here
+        auto ans=solve(root);
+        return ans.second;
+        
     }
 };
