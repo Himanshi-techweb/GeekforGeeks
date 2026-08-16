f8d787c6612ca244668c82952513473ec4b2d267
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-16 21:52:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Largest BST/largest-bst.cpp
+++ Difficulty: Medium/Largest BST/largest-bst.cpp
@@ -1,36 +1,36 @@
-/* Tree node structure  used in the program
-
-struct Node {
+/* Structure of a Binary Search Tree node
+class Node {
+public:
     int data;
-    Node *left;
-    Node *right;
-
+    Node *left, *right;
     Node(int val) {
         data = val;
-        left = right = NULL;
+        left = right = nullptr;
     }
 };*/
 
 class Solution {
   public:
     int ans=0;
-    pair<int,pair<int,int>> solve(Node* root){//sizez,maxi,mini
-        if(root==NULL)return {0,{INT_MIN,INT_MAX}};
-        if(!root->left && !root->right)return {1,{root->data,root->data}};
+    pair<int,pair<int,int>> solve(Node* root){
+        //return three thing how many nodes count and minimum and maximum
+        if(root==NULL)return {0,{INT_MAX,INT_MIN}};
         auto l=solve(root->left);
         auto r=solve(root->right);
-        if(l.second.first<root->data && r.second.second>root->data){
-            return {l.first+r.first+1,{max(root->data,r.second.first),min(root->data,l.second.second)}};
+        
+        //
+        if(l.second.second<root->data && r.second.first>root->data){
+           int x=l.first+r.first+1;
+           ans=max(ans,x);
+           int mn=min(l.second.first,root->data);
+           int mx=max(r.second.second,root->data);
+           return {x,{mn,mx}};
         }
-        else{
-            return {max(l.first,r.first),{INT_MAX,INT_MIN}};
-        }
+        else return {1,{INT_MIN,INT_MAX}};
     }
-    /*You are required to complete this method */
-    // Return the size of the largest sub-tree which is also a BST
     int largestBst(Node *root) {
-        // Your code here
-        auto ans=solve(root);
-        return ans.first;
+        // code here
+        solve(root);
+        return ans;
     }
 };
