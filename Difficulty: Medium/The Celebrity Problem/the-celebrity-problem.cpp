6f23a785d5c341c64d54724353887edb89c1dd3b
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-08-23 22:53:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -1,23 +1,31 @@
 class Solution {
   public:
-    int celebrity(vector<vector<int> >& mat) {
+    int celebrity(vector<vector<int>>& mat) {
         // code here
-        vector<int>  row;
-        int n=mat.size();
-        int t=0;int b=n-1;
-        while(t<b){
-            if(t!=b && mat[t][b]==1)t++; 
-            else if(t!=b && mat[b][t]==1)b--;
-            else {
-                t++;b--;
+        int m,n;
+        m=mat.size();n=mat[0].size();
+        vector<int> arr(n,0);
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1)arr[j]++;
             }
         }
-        for(int i=0;i<n;i++){
-            if(i!=t && mat[i][t]==0)return -1;
-        }
+        vector<int> arr1(n,0);
         for(int j=0;j<n;j++){
-            if(j!=t && mat[t][j]==1)return -1;
+            for(int i=0;i<m;i++){
+                if(mat[i][j]==1)arr1[i]++;
+            }
         }
-        return t;
+        int ans=-1;
+        int cnt=0;
+        for(int j=0;j<n;j++){
+            
+            if(arr[j]==m && arr1[j]==1 ){
+                ans=j;
+                cnt++;
+            }
+        }
+        
+        return (cnt==1)?ans:-1;
     }
 };
