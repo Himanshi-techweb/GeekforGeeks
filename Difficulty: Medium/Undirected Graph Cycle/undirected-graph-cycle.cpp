12f5f12033bb38f88149601764fe65408045589d
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-01 16:17:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -1,34 +1,33 @@
 class Solution {
   public:
-    //bool condition=false;bool condition2=false;
-    
-    bool isCycle(int V, vector<vector<int>>& edge) {
+    bool isCycle(int V, vector<vector<int>>& edges) {
         // Code here
-       vector<vector<int>> adj(V+1);
-       for(auto it:edge){
-           adj[it[0]].push_back(it[1]);
-           adj[it[1]].push_back(it[0]);
-       }
-       vector<int> visit(V+1);
-       for(int i=0;i<=V;i++){
-           queue<pair<int,int>> q;
-        q.push({i,-1});//node,parent
-           if(!visit[i]){
-            visit[i]=1;
-            while(!q.empty()){
-           int node=q.front().first;int parent=q.front().second;
-           q.pop();
-           for(auto it:adj[node]){
-              if(!visit[it]){
-                  visit[it]=1;
-                  q.push({it,node});
-              }
-              else if(parent!=-1 && it!=parent)return true;
-           }
-       }   
-           }
-       }
-       
-       return false;
+        vector<vector<int>> adj(V);
+        for(auto it:edges){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        queue<pair<int,int>> q;
+        vector<bool>visit(V,false);
+        for(int i=0;i<V;i++){
+            if(!visit[i]){
+                visit[i]=true;
+                q.push({i,-1});
+                while(!q.empty()){
+                    auto front =q.front();
+                    int node=front.first;
+                    int actual=front.second;
+                    q.pop();
+                    for(auto it:adj[node]){
+                        if(!visit[it]){
+                            q.push({it,node});
+                            visit[it]=true;
+                        }
+                        else if(it!=actual)return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 };
