d70e72230ec1c43d41c33e0bbe1511e3a2e8413a
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-01 16:38:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
+++ Difficulty: Medium/Distance of nearest cell having 1/distance-of-nearest-cell-having-1.cpp
@@ -1,48 +1,35 @@
 class Solution {
   public:
-    // void dfs(int i,int j,int row,int col,vector<vector<int>> &arr,vector<vector<int>>&vis,vector<vector<int>>&ans){
-    //   vis[i][j]=1;
-    //   for(int k=0;k<4;k++){
-    //       int nr=i+rc[k][0];
-    //       int nc=j+rc[k][1];
-    //       if(nr>=0 &&nr<row && nc>=0 && nc<col &&vis[nr][nc]==0){
-               
-    //       }
-    //   }
-    // }
-    // Function to find distance of nearest 1 in the grid for each cell.
-    vector<vector<int>> nearest(vector<vector<int>>& arr) {
-        // Code here
-        int row=arr.size();
-        int col=arr[0].size();
-        vector<vector<int>> ans(row,vector<int>(col,INT_MAX));
-        vector<vector<int>> vis(row,vector<int>(col,0));
-        queue<pair<pair<int,int>,int>> q;
-        vector<vector<int>> rc={{0,-1},{-1,0},{0,1},{1,0}};
-        
-        for(int i=0;i<row;i++){
-           for(int j=0;j<col;j++){
-            if(arr[i][j]==1){
-                q.push({{i,j},0});
-                vis[i][j]=1;
+    int m,n;
+    vector<vector<int>> rc={{-1,0},{1,0},{0,-1},{0,1}};
+    vector<vector<int>> nearest(vector<vector<int>>& grid) {
+        // code here
+        m=grid.size();n=grid[0].size();
+        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+        queue<pair<int,int>> q;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                    dist[i][j]=0;
+                }
             }
-           } 
         }
+        // queue<pair<int,int>> q;
         while(!q.empty()){
-          auto front=q.front();q.pop();
-          int r=front.first.first;
-          int c=front.first.second;
-          int dist=front.second;
-          ans[r][c]=min(ans[r][c],dist);
-          for(int i=0;i<4;i++){
-              int nr=r+rc[i][0];
-              int nc=c+rc[i][1];
-              if(nr>=0 && nr<row && nc>=0 && nc<col && vis[nr][nc]==0){
-                  q.push({{nr,nc},dist+1});
-                  vis[nr][nc]=1;
-              }
-          }
+            int r=q.front().first;
+            int c=q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++){
+                int nr=r+rc[i][0];int nc=c+rc[i][1];
+                if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]==0){
+                    if(dist[nr][nc]>dist[r][c]+1){
+                        dist[nr][nc]=dist[r][c]+1;
+                        q.push({nr,nc});
+                    }
+                }
+            }
         }
-        return ans;
+        return dist;
     }
 };
