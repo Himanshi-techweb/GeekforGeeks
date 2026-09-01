ad83e964bd75c4d95221bdfde5269dd16e93c2dc
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-01 17:16:03 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
+++ Difficulty: Medium/Number of Distinct Islands/number-of-distinct-islands.cpp
@@ -1,37 +1,46 @@
-// User function Template for C++
 class Solution {
   public:
-    vector<vector<int>> rc={{0,-1},{-1,0},{0,1},{1,0}};
-    void dfs(int i,int j,int i_base,int j_base,int m,int n,vector<vector<int>>& arr,vector<vector<int>> &visit,vector<pair<int,int>> &check){
-        visit[i][j]=1;
-        int ni=i-i_base;
-        int nj=j-j_base;
-        check.push_back({ni,nj});
-        for(auto it:rc){
-            int nr=i+it[0];
-            int nc=j+it[1];
-            if(nr>=0 && nr<m && nc>=0 && nc<n && !visit[nr][nc] && arr[nr][nc]==1){
-                visit[nr][nc]=1;
-                dfs(nr,nc,i_base,j_base,m,n,arr,visit,check);
-            }
-        }
-    }
-    int countDistinctIslands(vector<vector<int>>& arr) {
+    unordered_set<string>st ;
+    vector<vector<int>> rc={{-1,0},{0,-1},{0,1},{1,0}};
+    int m,n;
+    int countDistinctIslands(vector<vector<char>>& grid) {
         // code here
-        int m=arr.size();
-        int n=arr[0].size();
-        vector<vector<int>> visit(m,vector<int> (n,0));
-        set<vector<pair<int,int>>> st;
+        m=grid.size();n=grid[0].size();
+        vector<vector<bool>> visit(m,vector<bool>(n,false));
+        queue<pair<int,int>> q;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(!visit[i][j] && arr[i][j]==1){
-                    vector<pair<int,int>> check;
-                    dfs(i,j,i,j,m,n,arr,visit,check);
-                    sort(check.begin(),check.end());
-                    st.insert(check);
+                if(!visit[i][j] && grid[i][j]=='L'){
+                    q.push({i,j});
+                    string str="";
+                    str+=to_string(i-i);
+                    str+="+";
+                    str+=to_string(j-j);
+                    str+="+";
+                    while(!q.empty()){
+                        auto r=q.front().first;
+                        auto c=q.front().second;
+                        visit[r][c]=true;
+                        q.pop();
+                        for(int k=0;k<4;k++){
+                            int nr=r+rc[k][0];int nc=c+rc[k][1];
+                            if(nr>=0 && nr<m && nc>=0 && nc<n && grid[nr][nc]=='L' && !visit[nr][nc]){
+                                q.push({nr,nc});
+                                nr=nr-i;
+                                nc=nc-j;
+                                str+=to_string(nr);
+                                str+="+";
+                                str+=to_string(nc);
+                                str+="+";
+                            }
+                        }
+                    }
+                    st.insert(str);
                 }
             }
         }
+        
         return st.size();
+        
     }
 };
