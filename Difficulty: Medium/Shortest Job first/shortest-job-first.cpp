73b28230d3c96e6f530ddac3f74ed97b11288383
Himanshi-techweb <namdevhimanshi2003@gmail.com>
2026-09-07 17:21:07 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
+++ Difficulty: Medium/Shortest Job first/shortest-job-first.cpp
@@ -1,17 +1,15 @@
-// User function Template for C++
-
-//Back-end complete function Template for C++
-
 class Solution {
   public:
-    long long solve(vector<int>& bt) {
+    int solve(vector<int>& bt) {
         // code here
         sort(bt.begin(),bt.end());
-        long long cnt=0;long  sum=0;
+        int cnt=0;int sum=0;
         for(int i=0;i<bt.size()-1;i++){
-          cnt+=bt[i];
-          sum+=cnt;
+            sum+=cnt;
+            cnt+=bt[i];
+            
         }
+        sum+=cnt;
         return sum/bt.size();
     }
 };
